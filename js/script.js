6e01fe2a35fa829798bc8dd29e00fc5167897ed4
Moisés Macías Díaz <moises@mardesin.com>
2025-03-24 20:33:59 +0100

    Presentación Js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("hola");
+
+const alumno = "Moisés";
+let edad = 20;
+let direccion = "Barcelona";
+let telefono = "666777888";
+let Edad; // Variable sin inicializar
+
+Edad = edad + 1;
+
+console.log(edad);
+console.log(Edad);
